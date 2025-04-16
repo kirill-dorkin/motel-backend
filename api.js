324ac4990663226a2240e62ccfb10508dc3c152a
Morfi <morfi@Morfis-MacBook-Pro.local>
2025-04-16 15:13:40 +0600

    feat: deploy-ready backend with .env setup

--- api.js
+++ api.js
@@ -44,7 +44,7 @@ const port = 5000;
 
 async function main() {
   await mongoose.connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.in81gjk.mongodb.net/motel-develpoment-db`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nyywshe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   );
   try {
     app.listen(port, () => {
